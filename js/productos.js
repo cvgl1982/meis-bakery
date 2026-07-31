5c9a7b29ea1e5b51bb2494def43af50559120091
Carlos Gavino <cgavino@corp.eluniverso.com>
2026-07-31 16:22:30 -0500

    Corregir rutas de imágenes

--- js/productos.js
+++ js/productos.js
@@ -4,34 +4,34 @@ const productos = [
     nombre: "Clásica",
     descripcion: "Nuestra cookie tradicional, suave por dentro y ligeramente crujiente por fuera.",
     precio: 1.50,
-    imagen: "assets/cookies/clasica.jpg"
+    imagen: "assets/cookies/Clasica.jpg"
   },
   {
     id: 2,
     nombre: "Pistacho",
     descripcion: "Cookie artesanal con delicioso sabor a pistacho.",
     precio: 1.50,
-    imagen: "assets/cookies/pistacho.jpg"
+    imagen: "assets/cookies/Pistacho.jpg"
   },
   {
     id: 3,
     nombre: "Tango",
     descripcion: "Cookie inspirada en la clásica galleta Tango.",
     precio: 1.50,
-    imagen: "assets/cookies/tango.jpg"
+    imagen: "assets/cookies/Tango.jpg"
   },
   {
     id: 4,
     nombre: "Oreo",
     descripcion: "Cookie con trozos de Oreo y chocolate blanco.",
     precio: 1.50,
-    imagen: "assets/cookies/oreo.jpg"
+    imagen: "assets/cookies/Oreo.jpg"
   },
   {
     id: 5,
     nombre: "Red Velvet",
     descripcion: "Cookie Red Velvet con un delicioso toque de cacao.",
     precio: 1.50,
-    imagen: "assets/cookies/redvelvet.jpg"
+    imagen: "assets/cookies/RedVelvet.jpg"
   }
 ];
